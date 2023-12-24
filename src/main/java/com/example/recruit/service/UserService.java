892b16264901ae59c3e9febb6dd0a4da0af2d143
安然的尾巴 <3247187440@qq.com>
2023-12-24 20:30:45 +0800

    Initial commit

---
+++ src/main/java/com/example/recruit/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.recruit.service;/**
+ * @email 3247187440@qq.com
+ * @author lldwb
+ * @date 2023/12/23
+ * @time 22:21
+ * @PROJECT_NAME recruit
+ */public interface UserService {
+}
