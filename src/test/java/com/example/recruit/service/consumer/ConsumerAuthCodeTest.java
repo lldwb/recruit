9484ed27d5cbeb6fd8e6dcca48c05fb971f8eeb2
安然的尾巴 <3247187440@qq.com>
2024-03-01 10:17:24 +0800

    调试验证码服务

---
+++ src/test/java/com/example/recruit/service/consumer/ConsumerAuthCodeTest.java
@@ -0,0 +1,47 @@
+package com.example.recruit.service.consumer;
+
+import com.example.recruit.config.RabbitAuthCode;
+import com.example.recruit.config.RabbitConfig;
+import com.example.recruit.controller.LoginController;
+import com.example.recruit.domain.User;
+import com.example.recruit.dto.AuthCode;
+import jakarta.security.auth.message.AuthException;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author lldwb
+ * @email 3247187440@qq.com
+ * @date 2024/3/1
+ * @time 9:47
+ * @PROJECT_NAME recruit
+ */
+@SpringBootTest
+class ConsumerAuthCodeTest {
+    @Autowired
+    private ConsumerAuthCode consumerAuthCode;
+    @Autowired
+    private RabbitTemplate template;
+    @Autowired
+    private LoginController loginController;
+
+    @Test
+    void sendEmailAuthCode() throws AuthException {
+        AuthCode code = new AuthCode();
+        code.setSubject("自己");
+        code.setReceivingUser("17870143604");
+        User user = new User();
+        user.setUserPhone(17870143604L);
+        // 接口调用
+        loginController.sendAuthCode(user);
+        // 消息队列调用
+//        template.convertAndSend(RabbitConfig.EXCHANGE_NAME, RabbitAuthCode.ROUTING_KEY, code);
+        // 直接调用
+//        consumerAuthCode.sendEmailAuthCode(code);
+    }
+}
